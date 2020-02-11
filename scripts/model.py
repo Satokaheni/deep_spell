37187ee2df2b8e37147b84456d7a436d58835a54
Satokaheni <zach.stallbohm@gmail.com>
2020-02-11 17:57:16 +0000

    finished model and util scripts

---
+++ scripts/model.py
@@ -0,0 +1,35 @@
+from keras.models import Model
+from keras import layers
+
+
+# function to create model
+def build_model(max_input_len, input_vocab_size, max_output_len, output_vocab_len):
+    
+    encoder_input = layers.Input((max_input_len,))
+    decoder_input = layers.Input((max_output_len,))
+
+    encoder = layers.Embedding(input_vocab_size, 128, input_length=max_input_len, mask_zero=True)(encoder_input)
+
+    encoder = layers.Bidirectional(layers.LSTM(128, return_sequences=True, return_state=True, unroll=True), merge_mode='concat')(encoder)
+    encoder_outs, forward_h, forward_c, backward_h, backward_c = encoder
+    encoder_h = layers.concatenate([forward_h, backward_h])
+    encoder_c = layers.concatenate([forward_c, backward_c])
+
+    decoder = layers.Embedding(output_vocab_len, 2*128, input_length=max_output_len, mask_zero=True)(decoder_input)
+
+    decoder = layers.LSTM(2*128, return_sequences=True, unroll=True)(decoder, initial_state=[encoder_h, encoder_c])
+
+    attention = layers.dot([decoder, encoder_outs], axes=[2,2])
+    attention = layers.Activation('softmax', name='attention')(attention)
+
+    context = layers.dot([attention, encoder_outs], axes=[2,1])
+
+    decoder_combined_context = layers.concatenate([context, decoder])
+
+    output = layers.TimeDistributed(layers.Dense(128, activation='tanh'))(decoder_combined_context)
+    output = layers.TimeDistributed(layers.Dense(output_vocab_len, activation='softmax'))(output)
+
+    model = Model(inputs=[encoder_input, decoder_input], outputs=[output])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
--- scripts/utils.py
+++ scripts/utils.py
@@ -4,6 +4,8 @@ import spacy
 
 nlp = spacy.load('en_core_web_sm')
 CHARS = 'abcdefghijklmnopqrstuvwxyz'
+START_CHAR = 1
+PAD_CHAR = 0
 
 
 # Create Encoding and Decoding Dictionary for inputs and ouputs
@@ -45,7 +47,7 @@ def preprocess_sentence(sentence):
     # We are going to replace all proper nouns with special tokens
     # also we are going to lowercase
     sent = nlp(sentence)
-    return ' '.join([w.text.lower() if w.tag_ != 'NNP' else '<NNP>' for w in sent])
+    return ' '.join([w.text.lower() if w.tag_ != 'NNP' else '<nnp>' for w in sent])
 
 
 # function to generate spelling errors
@@ -64,13 +66,168 @@ def generate_errors(sentence):
     # determine which errors to create
     errors = np.random.choice([1, 2, 3, 4], size=number_imps)
 
-    # remove a space
-    # randomly choose a space to remove
+    # split the sentence by spaces to make it easier for choices
     sent_split = sentence.split()
-    remove = np.random.randint(0, len(sent_split)-1)
 
-    sentence = ' '.join(sent_split[:remove+1]) + ' '.join(sent_split[remove+1:])
-
-    # Remove a character from a word
-    # randomly choose a word
+    # randomly choose a words for the errors that will happen
+    # allow replacement multiple mispellings for the same word
+    non_space_errors = [e for e in errors if e != 1]
     
+    # remove <nnp> special tokens as choices
+    allowed_words = []
+    for i in range(len(sent_split)):
+        if sent_split[i] != '<nnp>':
+            allowed_words.append(i)
+    
+    word_choices = np.random.choice(allowed_words, size=len(non_space_errors))
+
+    # iterate through words being misspelled
+    for word in word_choices:
+
+        # randomly choose an option of remove, swap or add
+        option = np.random.randint(1, 3)
+        
+        # randomly remove a character
+        if option == 1:
+            char_choice = np.random.randint(0, len(sent_split[word]))
+            sent_split[word] = sent_split[word][:char_choice] + sent_split[word][char_choice+1:]
+
+        # randomly swap two characters 
+        elif option == 2:
+            char_choice = np.random.randint(0, len(sent_split[word])-1)
+            sent_split[word] = sent_split[word][:char_choice] + sent_split[word][char_choice+1] + sent_split[word][char_choice] + sent_split[char_choice+2:]
+
+        # randomly add a character
+        else:
+            char_choice = np.random.randint(0, len(sent_split[word]))
+            added_char = int(np.random.randint(0, 26, size=1))
+            added_char = CHARS[added_char]
+            sent_split[word] = sent_split[word][:char_choice] + added_char + sent_split[char_choice:]
+
+    # remove spaces last
+    # remove a space/spaces
+    space_removals = [e for e in errors if e == 1]
+    for _ in space_removals:
+
+        # randomly choose a space to remove
+        remove = np.random.randint(0, len(sent_split)-1)
+        sentence = ' '.join(sent_split[:remove+1]) + ' '.join(sent_split[remove+1:])
+        sent_split = sentence.split()
+
+    return ' '.join(sent_split)
+
+
+# Create training data
+def create_training_data(input_data):
+    
+    # preprocess data
+    preproccesed_data = [preprocess_sentence(x) for x in input_data]
+
+    # create spelling errors
+    encoder_data = [generate_errors(x) for x in preproccesed_data]
+    target_data = preproccesed_data
+
+    # encoder input
+    encoding_w2id, encoding_id2w, encoding_dict_len = create_encode_decode_tables(encoder_data)
+    encoder_data = encode_sentences(encoding_w2id, encoder_data, max_sent_length=200)
+
+    # target output
+    decoder_w2id, decoder_id2w, decoder_dict_len = create_encode_decode_tables(target_data)
+    target_data = encode_sentences(decoder_w2id, target_data, max_sent_length=200)
+
+    # decoder 
+    decoder_input = np.zeros_like(target_data)
+    decoder_input[:, 1:] = encoder_data[:, :-1]
+    decoder_input[:, 0] = START_CHAR
+    decoder_output = np.eye(decoder_dict_len)[target_data.astype('int')]
+
+    x = [encoder_data, decoder_input]
+    y = [decoder_output]
+
+    return x, y, {'id2w': encoding_id2w, 'w2id': encoding_w2id}, {'id2w': decoder_id2w, 'w2id': decoder_w2id}
+
+
+# function to generate encoded outputs for inputs for testing
+def generate(texts, encoding_w2id, model, max_input_len, max_output_len, beam_size=3, max_beams=3, min_cut_off_len=10, cut_off_ratio=1.5):
+
+    # choose one
+    # min_cut_off_len = max(min_cut_off_len, cut_off_ratio*len(max(texts, key=len)))
+    min_cut_off_len = min(min_cut_off_len, max_output_len)
+
+    all_completed_beams = {i: [] for i in range(len(texts))}
+    all_running_beams = {}
+
+    for i, text in enumerate(texts):
+        all_running_beams[i] = [[np.zeros((len(text), max_output_len)), [1]]]
+        all_running_beams[i][0][0][:, 0] = START_CHAR
+
+    while len(all_running_beams) != 0:
+        for i in all_running_beams:
+            all_running_beams[i] = sorted(all_running_beams[i], key=(lambda tup: np.prod(tup[1])), reverse=True)
+            all_running_beams[i] = all_running_beams[i][:max_beams]
+
+        in_out_map = {}
+        batch_encoder_input = []
+        batch_decoder_input = []
+
+        t_c = 0 
+        for text_i in all_running_beams:
+            if text_i not in in_out_map:
+                in_out_map[text_i] = []
+            for running_beam in all_running_beams[text_i]:
+                in_out_map[text_i].append(t_c)
+                t_c += 1
+                batch_encoder_input.append(texts[text_i])
+                batch_decoder_input.append(running_beam[0][0])
+
+        batch_encoder_input = encode_sentences(encoding_w2id, batch_encoder_input, max_input_len)
+        batch_decoder_input = np.asarray(batch_decoder_input)
+        batch_predictions = model.predict([batch_encoder_input, batch_decoder_input])
+
+        t_c = 0
+        for text_i, _ in in_out_map.items():
+            temp_running_beams = []
+            for running_beam, probs in all_running_beams[text_i]:
+                if len(probs) >= min_cut_off_len:
+                    all_completed_beams[text_i].append([running_beam[:, 1:], probs])
+                else:
+                    prediction = batch_predictions[t_c]
+                    sorted_args = prediction.argsort()
+                    sorted_probs = np.sort(prediction)
+
+                    for i in range(1, beam_size+1):
+                        temp_running_beam = np.copy(running_beam)
+                        i = -1 * i
+                        ith_arg = sorted_args[:, i][len(probs)]
+                        ith_prob = sorted_probs[:, i][len(probs)]
+
+                        temp_running_beam[:, len(probs)] = ith_arg
+                        temp_running_beams.append([temp_running_beam, probs + [ith_prob]])
+                    
+                t_c += 1
+
+            all_running_beams[text_i] = [b for b in temp_running_beams]
+
+        to_del = []
+        for i, v in all_running_beams.items():
+            if not v:
+                to_del.append(i)
+
+        for i in to_del:
+            del all_running_beams[i]
+
+    return all_completed_beams
+
+
+# function to generate outputs for given inputs
+def infer(texts, model, encoding_w2id, decoding_id2w, max_input_len, max_output_len):
+
+    all_decoder_outputs = generate(texts, encoding_w2id, model, max_input_len, max_output_len)
+    outputs = []
+
+    for _, decoder_outputs in all_decoder_outputs.items():
+        outputs.append([])
+        for decoder_output, probs in decoder_outputs:
+            outputs[-1].append({'sequence': decode_sentence(decoding_id2w, decoder_output[0]), 'prob': np.prod(probs)})
+
+    return outputs
